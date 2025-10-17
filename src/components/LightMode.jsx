f5224a59bea1a7e2292da773713fa850093244ee
aliNzLami <ali.nabizadeh79@yahoo.com>
2025-10-17 16:43:35 +0330

    switch dark and light mode operation

--- src/components/LightMode.jsx
+++ src/components/LightMode.jsx
@@ -1,8 +1,42 @@
 'use client'
 import icon from '@/assets/pics/lightDark.png'
 import Image from 'next/image'
+import { useCallback, useEffect } from 'react';
 
 function LightMode() {
+
+    const changeDark = (isDark) => {
+        const mode = isDark ? "dark" : "light";
+        localStorage.setItem('lightMode', mode);
+        if(isDark) {
+            document.body.classList.add('darkMode');
+        }
+        else {
+            document.body.classList.remove('darkMode');
+        }
+    }
+
+    const checkLight = useCallback(() => {
+        const mode = localStorage.getItem('lightMode');
+        if(!mode) {
+            changeDark(false);
+        }
+        else {
+            if(mode === 'dark') {
+                changeDark(false);
+            }
+            else {
+                changeDark(true);
+            }
+        }
+    }, [])
+
+    useEffect(() => {
+        const mode = localStorage.getItem('lightMode');
+        if(mode === 'dark') changeDark(true)
+    }, [])
+    
+    
     return (
         <Image  
             src={icon.src}
@@ -10,6 +44,7 @@ function LightMode() {
             width={25}
             height={25}
             className='cursor-pointer'
+            onClick={checkLight}
         />
     )
 }
